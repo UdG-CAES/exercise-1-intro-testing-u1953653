c7725ed0a79a82b9fa43626c64ef7b3e16ac941f
Kevin Costa <>
2020-10-05 17:17:41 +0200

    Nous testos que passen amb bugs en les funciones

--- src/test/java/edu/udg/caes/ExerciseIntroTestingTest.java
+++ src/test/java/edu/udg/caes/ExerciseIntroTestingTest.java
@@ -29,4 +29,32 @@ public class ExerciseIntroTestingTest {
         final int result = ExerciseIntroTesting.oddOrPos(new int []{-3, -2, 0, 1, 4});
         assertEquals(result, 3);
     }
+
+    // ------------------
+    // Testos nous
+    // ------------------
+
+    @Test
+    public void testFindLastV2(){
+        final int result = ExerciseIntroTesting.findLast(new int []{2, 3, 5}, 3);
+        assertEquals(result, 1);
+    }
+
+    @Test
+    public void testLastZeroV2(){
+        final int result = ExerciseIntroTesting.lastZero(new int []{0, 1, 2});
+        assertEquals(result, 0);
+    }
+
+    @Test
+    public void testCountPositiveV2(){
+        final int result = ExerciseIntroTesting.countPositive(new int []{-4, -2, 1, 2});
+        assertEquals(result, 2);
+    }
+
+    @Test
+    public void testOddOrPosV2(){
+        final int result = ExerciseIntroTesting.oddOrPos(new int []{-3, -3, 0, 1});
+        assertEquals(result, 1);
+    }
 }
